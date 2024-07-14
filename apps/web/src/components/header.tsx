9a75a77f8081d6c867d591ca00bf9d32ff2978ad
GSouzap <gabriel_wow17@hotmail.com>
2024-07-14 16:39:25 -0300

    feat: create dropdown menu for organizations and verify roles for projects

---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,28 @@
+import nextIcon from '@/assets/next-icon.svg'
+import Image from 'next/image'
+import { ProfileButton } from './profile-button'
+import { Slash } from 'lucide-react'
+import { OrganizationSwitcher } from './organization-switcher'
+import { ability } from '@/auth/auth'
+
+export async function Header() {
+  const permissions = await ability()
+
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image src={nextIcon} className='size-6 dark:invert' alt='Nextjs Logo'/>
+
+        <Slash className='size-3 -rotate-[24deg] text-border' />
+
+        <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>Projects</p>}
+      </div>
+
+      <div className='flex items-center gap-4'>
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
