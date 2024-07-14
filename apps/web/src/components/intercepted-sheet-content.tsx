42e0013f9130f52ab23019edf1f44e5a0ce2671f
GSouzap <gabriel_wow17@hotmail.com>
2024-07-14 19:59:52 -0300

    feat: add create new project with parallel routes

---
+++ apps/web/src/components/intercepted-sheet-content.tsx
@@ -0,0 +1,45 @@
+import * as React from "react"
+import * as SheetPrimitive from "@radix-ui/react-dialog"
+import { type VariantProps } from "class-variance-authority"
+import { X } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { SheetOverlay, SheetPortal, sheetVariants } from "./ui/sheet"
+import { useRouter } from "next/navigation"
+
+interface InterceptedSheetContentProps
+  extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
+  VariantProps<typeof sheetVariants> { }
+
+export const InterceptedSheetContent = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Content>,
+  InterceptedSheetContentProps
+>(({ side = "right", className, children, ...props }, ref) => {
+  const router = useRouter()
+
+  function onDissmis() {
+    router.back()
+  }
+
+  return (
+    (
+      <SheetPortal>
+        <SheetOverlay />
+        <SheetPrimitive.Content
+          onEscapeKeyDown={onDissmis}
+          onPointerDownOutside={onDissmis}
+          ref={ref}
+          className={cn(sheetVariants({ side }), className)}
+          {...props}
+        >
+          {children}
+          <button onClick={onDissmis} className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+            <X className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </button>
+        </SheetPrimitive.Content>
+      </SheetPortal>
+    )
+  )
+})
+InterceptedSheetContent.displayName = SheetPrimitive.Content.displayName
